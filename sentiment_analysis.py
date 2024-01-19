b20fd51b019e581cbf4278fa9b01540a98e54dbe
jiuyuanx <122936191+jiuyuanx@users.noreply.github.com>
2024-01-19 17:20:45 -0500

    add

---
+++ sentiment_analysis.py
@@ -0,0 +1,54 @@
+#%%
+# from huggingface_hub import notebook_login
+# notebook_login()
+#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
+#%%
+from transformers import AutoTokenizer, BertForSequenceClassification
+from torch.nn.functional import softmax
+import torch
+from data import load_data
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Device: ", device)
+#%%
+
+# Step 1: Load Pretrained Model and Tokenizer
+tokenizer = AutoTokenizer.from_pretrained("textattack/bert-base-uncased-yelp-polarity")
+model = BertForSequenceClassification.from_pretrained("textattack/bert-base-uncased-yelp-polarity")
+
+#%%
+# print(model)
+#%%
+def sentiment_analysis(text):
+    # Step 2: Preprocess Text
+    inputs = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors="pt")
+
+    # Step 3: Predict Sentiment
+    with torch.no_grad():
+        outputs = model(**inputs)
+        predictions = softmax(outputs.logits, dim=1)
+
+    # Mapping indices to sentiment
+    sentiment_map = {0: "Negative", 1: "Positive"}
+
+    # Step 4: Interpret Results
+    sentiment_score, sentiment_index = torch.max(predictions, dim=1)
+    sentiment_label = sentiment_map[int(sentiment_index)]
+
+    return sentiment_label, sentiment_score.item()
+
+# Example Usage
+# text = "I love it!"
+# sentiment, score = sentiment_analysis(text)
+# print(f"Sentiment: {sentiment}, Confidence: {score}")
+
+# %%
+from tqdm.auto import tqdm
+df = load_data()
+scores = []
+for i in tqdm(range(len(df))):
+    text = df.iloc[i]['content']
+    label, score = sentiment_analysis(text)
+    scores.append(score)
+df['sentiment'] = scores
+df
+# %%
